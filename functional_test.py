0a2ee388f0b344c7b11a85460658cbcdab679131
patymori <excermori@yahoo.com.br>
2017-03-05 10:55:03 -0300

    Change functional test

--- functional_test.py
+++ functional_test.py
@@ -3,10 +3,25 @@ Functional test.
 
 Django TDD - init project
 """
+import unittest
 
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    # Initial test to open the browser in the first page
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_must_open_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Simple Time Sheet', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
